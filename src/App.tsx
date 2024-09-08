f4da58abe4c18e71867615845357471c1cef2438
Gabriel Brilhante <ielcordeir070@gmail.com>
2024-09-08 09:27:51 -0300

    feat: app initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import { Navbar } from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
